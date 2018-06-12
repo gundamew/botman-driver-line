ca0ffc5829a5da72495d472f9a483db24536cf90
Bing-Sheng Chen <gundamew@gmail.com>
2018-06-12 16:38:57 +0800

    Add message event (video object) support

---
+++ src/LineMessageVideoDriver.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace BotMan\Drivers\Line;
+
+use BotMan\BotMan\Messages\Attachments\Video;
+use BotMan\BotMan\Messages\Incoming\IncomingMessage;
+
+class LineMessageVideoDriver extends LineDriver
+{
+    const DRIVER_NAME = 'LineMessageVideo';
+
+    /**
+     * Determine if the request is for this driver.
+     *
+     * @return bool
+     */
+    public function matchesRequest()
+    {
+        return ($this->event->get('type') === 'message')
+            && ($this->event->get('message')['type'] === 'video')
+            && $this->validateSignature();
+    }
+
+    /**
+     * Retrieve the chat message.
+     *
+     * @return array
+     */
+    public function getMessages()
+    {
+        if (empty($this->messages)) {
+            $this->messages = [new IncomingMessage(
+                Video::PATTERN,
+                $this->event->get('source')['userId'],
+                $this->event->get('source')['userId'],
+                $this->payload
+            )];
+        }
+
+        return $this->messages;
+    }
+
+    /**
+     * @return string
+     */
+    public function getVideoContent()
+    {
+        $endpoint = str_replace(
+            '{messageId}',
+            urlencode($this->event->get('message')['id']),
+            '/message/{messageId}/content'
+        );
+
+        return $this->http->get($this->getApiUrl($endpoint), [], [
+            'Authorization: Bearer ' . $this->config->get('channel_access_token'),
+        ])->getContent();
+    }
+}
