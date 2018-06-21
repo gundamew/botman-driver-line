04704a1770e4e93547c38c3b7aa648e80ee2a5ba
Bing-Sheng Chen <gundamew@gmail.com>
2018-06-21 20:12:45 +0800

    Add image carousel template message support

---
+++ src/Extensions/ImageCarouselTemplate.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace BotMan\Drivers\Line\Extensions;
+
+class ImageCarouselTemplate extends AbstractTemplate
+{
+    /** @var array */
+    protected $columns = [];
+
+    /**
+     * @param ImageCarouselColumn $column
+     * @return $this
+     */
+    public function addColumn(ImageCarouselColumn $column)
+    {
+        $this->columns[] = $column->toArray();
+
+        return $this;
+    }
+
+    /**
+     * @param array $columns
+     * @return $this
+     */
+    public function addColumns(array $columns)
+    {
+        foreach ($columns as $column) {
+            $this->columns[] = $column->toArray();
+        }
+
+        return $this;
+    }
+
+    /**
+     * @return array
+     */
+    public function toArray()
+    {
+        return [
+            'type' => 'template',
+            'altText' => $this->text,
+            'template' => [
+                'type' => 'image_carousel',
+                'columns' => $this->columns,
+            ],
+        ];
+    }
+}
